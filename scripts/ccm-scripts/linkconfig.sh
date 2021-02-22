c9969d01bf041148d7c96cb050545d6130379e4c
roland-sipos <sipos.roland.cb@gmail.com>
2021-02-22 13:13:04 +0100

    Small corrections

--- scripts/ccm-scripts/linkconfig.sh
+++ scripts/ccm-scripts/linkconfig.sh
@@ -2,15 +2,16 @@
 
 if [ "$#" -ne 3 ] || [ "$1" == "-h" ]
 then
-  echo "Usage 'linkconfig.sh cardid N M' with N and M as number of enabled emu links on the endpoints"
+  echo "Usage 'linkconfig.sh cardid slr1-config-file slr2-config-file' "
 else
   cardid=$1
+  rmfile1=$2
+  rmfile2=$3
   echo "card id" $cardid
-  flx-config -d $cardid load emu-5links-slr1
+  flx-config -d $cardid load $rmfile1
   femu -d $cardid -n
 
   echo "card id" $((cardid+1))
-  flx-config -d $((cardid+1)) load emu-5links-slr2
+  flx-config -d $((cardid+1)) load $rmfile2
   femu -d $((cardid+1)) -n
-
 fi
