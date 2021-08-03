57f6f9f132f9bec26b79bd149f438ba7f9f7b388
roland-sipos <sipos.roland.cb@gmail.com>
2021-08-03 13:02:27 +0200

    cleanup

--- apps/emu_confgen.cxx
+++ apps/emu_confgen.cxx
@@ -1,25 +1,33 @@
-#include <iostream>
+/**
+ * @file emu_confgen.cxx Generates files tad can be loaded by flx-config
+ *
+ * This is part of the DUNE DAQ , copyright 2020.
+ * Licensing/copyright details are in the COPYING file that you should have
+ * received with this code.
+ */
 #include <cstdint>
+#include <cstdlib>
 #include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
-#include <cstdlib>
-
-//uint32_t g_chunk_count;
 
 // IDLE=K28.5, SOP=K28.1, EOP=K28.6, SOB=K28.2, EOB=K28.3
 const constexpr uint64_t FM_KCHAR_IDLE = (((uint64_t)1 << 32) | 0xBC);
-const constexpr uint64_t FM_KCHAR_SOP  = (((uint64_t)1 << 32) | 0x3C);
-const constexpr uint64_t FM_KCHAR_EOP  = (((uint64_t)1 << 32) | 0xDC);
-const constexpr uint64_t FM_KCHAR_SOB  = (((uint64_t)1 << 32) | 0x5C);
-const constexpr uint64_t FM_KCHAR_EOB  = (((uint64_t)1 << 32) | 0x7C);
+const constexpr uint64_t FM_KCHAR_SOP = (((uint64_t)1 << 32) | 0x3C);
+const constexpr uint64_t FM_KCHAR_EOP = (((uint64_t)1 << 32) | 0xDC);
+const constexpr uint64_t FM_KCHAR_SOB = (((uint64_t)1 << 32) | 0x5C);
+const constexpr uint64_t FM_KCHAR_EOB = (((uint64_t)1 << 32) | 0x7C);
 
-#define CRC_WIDTH 20
-#define CRC_MASK ((1 << CRC_WIDTH) - 1)
-#define CRC_POLYNOM_1 0xC1ACF
-#define CRC_POLYNOM_2 0x8359F
-#define CRC_INITVAL 0xFFFFF
-#define CHUNKHDR_SIZE 8
+// CRC constants
+const constexpr uint64_t CRC_WIDTH = 20;
+const constexpr uint64_t CRC_MASK = ((1 << CRC_WIDTH) - 1);
+const constexpr uint64_t CRC_POLYNOM_1 = 0xC1ACF;
+const constexpr uint64_t CRC_POLYNOM_2 = 0x8359F;
+const constexpr uint64_t CRC_INITVAL = 0xFFFFF;
+
+// Chunk constants
+const constexpr uint64_t CHUNKHDR_SIZE = 8;
 
 uint64_t
 crc20(uint64_t* data, uint64_t length, bool crc_new)
@@ -27,36 +35,40 @@ crc20(uint64_t* data, uint64_t length, bool crc_new)
   // Initialize
   uint64_t crc = CRC_INITVAL;
   uint64_t polynomial;
-  if (crc_new)
+  if (crc_new) {
     polynomial = CRC_POLYNOM_2;
-  else
+  } else {
     polynomial = CRC_POLYNOM_1;
+  }
 
   unsigned int i, k;
   for (k = 0; k < CRC_WIDTH; ++k) {
-    if ((crc & 1))
+    if ((crc & 1)) {
       crc = (crc >> 1) ^ ((1 << (CRC_WIDTH - 1)) | (polynomial >> 1));
-    else
+    } else {
       crc = (crc >> 1);
+    }
   }
 
   // Calculate CRC
   for (i = 0; i < length; i++) {
     for (k = 1; k <= 32; k++) {
-      if (crc & (1 << (CRC_WIDTH - 1)))
+      if (crc & (1 << (CRC_WIDTH - 1))) {
         crc = ((crc << 1) | ((data[i] >> (32 - k)) & 1)) ^ polynomial;
-      else
+      } else {
         crc = ((crc << 1) | ((data[i] >> (32 - k)) & 1));
+      }
     }
     crc &= CRC_MASK;
   }
 
   // One more loop
   for (k = 0; k < CRC_WIDTH; k++) {
-    if (crc & (1 << (CRC_WIDTH - 1)))
+    if (crc & (1 << (CRC_WIDTH - 1))) {
       crc = (crc << 1) ^ polynomial;
-    else
+    } else {
       crc = (crc << 1);
+    }
   }
   crc &= CRC_MASK;
 
@@ -79,15 +91,16 @@ generateFm(uint64_t* emudata,
 {
   // Initialize emudata to all zeroes
   unsigned i;
-  for (i = 0; i < emusize; ++i)
+  for (i = 0; i < emusize; ++i) {
     emudata[i] = 0;
+  }
 
   // Determine the number of chunks that will fit
   // (chunk size includes 8-byte header): 2 IDLEs, SOP, chunk, EOP
   uint32_t max_chunkcnt = (emusize - 2) / (1 + req_chunksize / 4 + 1 + idle_chars);
   uint32_t index = 0;
   bool success = true;
-  //g_chunk_count = max_chunkcnt;
+  // g_chunk_count = max_chunkcnt;
 
   // Start with some IDLE symbols
   emudata[index++] = FM_KCHAR_IDLE;
@@ -96,8 +109,7 @@ generateFm(uint64_t* emudata,
   // Multiple chunks
   uint32_t next_index, chunkcntr = 0, chunksz, chunk_datasz;
   while (index < emusize && chunkcntr < max_chunkcnt) {
-    if (random_sz && req_chunksize > 8) // Size not less than 8
-    {
+    if (random_sz && req_chunksize > 8) { // Size not less than 8
       // Determine a random (data) size to use for the next chunk
       // (here: size between req_chunksize/2 and req_chunksize,
       //  but rounded up to a multiple of 4 bytes)
@@ -114,8 +126,9 @@ generateFm(uint64_t* emudata,
     next_index = index + (1 + chunksz / 4 + 1);
     if (next_index >= emusize) {
       // It won't fit, so forget it: from here onwards fill with IDLEs
-      for (; index < emusize; ++index)
+      for (; index < emusize; ++index) {
         emudata[index] = FM_KCHAR_IDLE;
+      }
       // Should exit the while-loop on the basis of the chunk counter
       // so we consider this an error...
       success = false;
@@ -124,59 +137,75 @@ generateFm(uint64_t* emudata,
 
     // SOP
     emudata[index++] = FM_KCHAR_SOP;
-    if (omit_one_soc && chunkcntr == 2)
+    if (omit_one_soc && chunkcntr == 2) {
       --index; // For testing
+    }
 
     // Add chunk header
     chunk_datasz = chunksz - CHUNKHDR_SIZE;
-    if (use_streamid)
+    if (use_streamid) {
       emudata[index++] = ((chunkcntr & 0xFF) | // Chunk counter = StreamID
                           (chunk_datasz & 0xF00) | ((chunk_datasz & 0x0FF) << 16) | ((chunkcntr & 0xFF) << 24));
-    else
+    } else {
       emudata[index++] = (0xAA | (chunk_datasz & 0xF00) | ((chunk_datasz & 0x0FF) << 16) | ((chunkcntr & 0xFF) << 24));
+    }
+
     emudata[index++] = 0x10AABB00; // ewidth=0x10=16 bits
 
     // Add chunk data according to 'pattern_id'
-    if (pattern_id == 1)
-      for (i = 0; i < chunk_datasz / 4; ++i)
+    if (pattern_id == 1) {
+      for (i = 0; i < chunk_datasz / 4; ++i) {
         emudata[index++] = 0xAA55AA55;
-    else if (pattern_id == 2)
-      for (i = 0; i < chunk_datasz / 4; ++i)
+      }
+    } else if (pattern_id == 2) {
+      for (i = 0; i < chunk_datasz / 4; ++i) {
         emudata[index++] = 0xFFFFFFFF;
-    else if (pattern_id == 3)
-      for (i = 0; i < chunk_datasz / 4; ++i)
+      }
+    } else if (pattern_id == 3) {
+      for (i = 0; i < chunk_datasz / 4; ++i) {
         emudata[index++] = 0x00000000;
-    else {
+      }
+    } else {
       unsigned int cntr = 0;
-      for (i = 0; i < chunk_datasz / 4; ++i, cntr += 4)
+      for (i = 0; i < chunk_datasz / 4; ++i, cntr += 4) {
         emudata[index++] = ((((cntr + 3) & 0xFF) << 24) | (((cntr + 2) & 0xFF) << 16) | (((cntr + 1) & 0xFF) << 8) |
                             (((cntr + 0) & 0xFF) << 0));
+      }
     }
 
     // EOP (+ 20-bits CRC)
     uint64_t crc = crc20(&emudata[index - chunksz / 4], chunksz / 4, crc_new);
-    if (add_crc_err && chunkcntr == 3)
-      ++crc; // For testing
-    emudata[index++] = FM_KCHAR_EOP | (crc << 8);
-    if (omit_one_eoc && chunkcntr == 2)
-      --index; // For testing
 
-    if (add_busy && chunkcntr == 0)
+    if (add_crc_err && chunkcntr == 3) {
+      ++crc; // For testing
+    }
+
+    emudata[index++] = FM_KCHAR_EOP | (crc << 8);
+
+    if (omit_one_eoc && chunkcntr == 2) {
+      --index; // For testing
+    }
+
+    if (add_busy && chunkcntr == 0) {
       emudata[index++] = FM_KCHAR_SOB;
+    }
 
     // A configurable number of comma symbols in between chunks
-    for (i = 0; i < idle_chars; ++i)
+    for (i = 0; i < idle_chars; ++i) {
       emudata[index++] = FM_KCHAR_IDLE;
+    }
 
-    if (add_busy && chunkcntr == 0)
+    if (add_busy && chunkcntr == 0) {
       emudata[index++] = FM_KCHAR_EOB;
+    }
 
     ++chunkcntr;
   }
 
   // Fill any remaining uninitialised array locations with IDLE symbols
-  for (; index < emusize; ++index)
+  for (; index < emusize; ++index) {
     emudata[index] = FM_KCHAR_IDLE;
+  }
 
   // We expect to have generated max_chunkcnt chunks!
   if (chunkcntr < max_chunkcnt) {
@@ -190,7 +219,7 @@ int
 main(int argc, char* argv[])
 {
 
-  /*"-h", "--help", "--filename", "--emuSize", "--chunkSize", "--idles", "--pattern"*/
+  // "-h", "--help", "--filename", "--emuSize", "--chunkSize", "--idles", "--pattern"
 
   const std::vector<std::string> cmdArgs = { argv,
                                              argv + argc }; // store arguments, options and flags from the command line
@@ -287,7 +316,7 @@ main(int argc, char* argv[])
              omit_one_eoc,
              add_crc_err);
 
-  for (int i = 0; i < emusize; i++) {
+  for (unsigned i = 0; i < emusize; i++) {
     output << "FE_EMU_CONFIG_WRADDR=0x" << std::hex << i << std::endl;
     output << "FE_EMU_CONFIG_WRDATA=0x" << std::hex << emudata[i] << std::endl;
     output << "FE_EMU_CONFIG_WE=1" << std::endl;
