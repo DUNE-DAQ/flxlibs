5cc759dba57ca7842146a04b242b5b4925ff05ce
Alessandro Thea <alessandro.thea@cern.ch>
2023-05-09 14:46:21 +0200

    First pass at movin from hardware map to detector readout map

--- python/flxlibs/cardcontrollerapp/cardcontrollerapp_gen.py
+++ python/flxlibs/cardcontrollerapp/cardcontrollerapp_gen.py
@@ -22,20 +22,20 @@ def get_cardcontroller_app(
         nickname,
         card_id=0,
         emulator_mode=False,
-        enable_firmware_tpg=False,
+        # enable_firmware_tpg=False,
         ignore_alignment_mask=([], []),
         host="localhost",
-        dro_info=None):
+        ru_desc=None):
     '''
     Here an entire application controlling one physical FLX card is generated. 
     '''
 
     # Get Elink infos for every SLR on this physical card.
     slrs = {}
-    for link in dro_info.links:
-        if not link.dro_slr in slrs:
-            slrs[link.dro_slr] = []
-        slrs[link.dro_slr].append(link.dro_link)
+    for link in ru_desc.links:
+        if not link.config.slr in slrs:
+            slrs[link.config.slr] = []
+        slrs[link.config.slr].append(link.config.link)
 
     # Sort elinks in each SLR
     for slr in slrs:
@@ -54,8 +54,8 @@ def get_cardcontroller_app(
         elinks = []
         for l in slrs[slr]:
             elinks.append(flx.Link(link_id=l, enabled=True, dma_desc=0, superchunk_factor=12))
-        if enable_firmware_tpg:
-            elinks.append(flx.Link(link_id=5, enabled=True, dma_desc=0, superchunk_factor=64))
+        # if enable_firmware_tpg:
+            # elinks.append(flx.Link(link_id=5, enabled=True, dma_desc=0, superchunk_factor=64))
         lus.append(flx.LogicalUnit(log_unit_id=slr, emu_fanout=emulator_mode, links=elinks, ignore_alignment_mask=ignore_alignment_mask[slr]))
 
     # Create modules
